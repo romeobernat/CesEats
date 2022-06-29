234008bc3772f2ed4b0ea4a6f4817d060d27f803
romeobernat <romeo.bernat@gmail.com>
2022-06-30 00:23:28 +0200

    fichier manquant

--- BackendNodeJS/TemplateBackEnd/lib/controllers/mongodb/articleCommande.controller.ts
+++ BackendNodeJS/TemplateBackEnd/lib/controllers/mongodb/articleCommande.controller.ts
@@ -1,25 +1,41 @@
 import ArticleCommande from "../../models/mongodb/articleCommande.model"
 
-export const getAllArticleCommande = async () => {
-    const articleCommandes = await ArticleCommande.find();
-    return articleCommandes;
-}
+export const getAllArticleCommande = async (req, res, next) => {
+    const allArticleCommande = await ArticleCommande.find();
+    return res
+      .status(200)
+      .json({ message: "Todo fetched successfully", data: allArticleCommande });
+  };
 
-export const createArticleCommande = (ArticleCommandeData:JSON) => {
-    const articleCommande = new ArticleCommande(ArticleCommandeData);
-    return articleCommande.save();
-}
+export const createArticleCommande = async (req, res, next) => {
+    var articleCommande = await ArticleCommande.create({ ...req.body });
+    return res
+      .status(200)
+      .json({ message: "Todo created successfully", data: articleCommande });
+  };
 
-export const getArticleCommandeById = async (req:any) => {
-    const articleCommande = await ArticleCommande.findById(req.params.restau_id);
-    return articleCommande;
-}
+export const getArticleCommandeById = async (req, res, next) => {
+    const { _id } = req.params;
+    const ArticleCommandeById = await ArticleCommande.findById(_id);
+    return res
+      .status(200)
+      .json({ message: "Todo fetched successfully", data: ArticleCommandeById });
+  };
 
-export const updateArticleCommande = async (req:any) => {
-    const articleCommande = await ArticleCommande.findByIdAndUpdate(req.params.id, req.body);
-    articleCommande.save();
-}
+export const updateArticleCommande = async (req, res, next) => {
+  const { _id } = req.params;
+  await ArticleCommande.findByIdAndUpdate({ _id }, { ...req.body });
+  const updatedArticleCommande = await ArticleCommande.findById(_id);
+  return res
+    .status(200)
+    .json({ message: "Todo updated successfully", data: updatedArticleCommande});
+};
 
-export const deleteArticleCommande = (req:any) => {
-    ArticleCommande.findByIdAndDelete(req.params.id)
+export const deleteArticleCommande = async (req, res, next) => {
+  const { _id } = req.params;
+  const deletedTodo = await ArticleCommande.findById(_id);
+  await ArticleCommande.findByIdAndDelete(_id);
+  return res
+    .status(200)
+    .json({ message: "Todo deleted successfully", data: deletedTodo });
 };
